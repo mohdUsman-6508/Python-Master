759eb305139deeee64cd5ddb17c5777bb2e82d30
Mohammad Usman <uk351675@gmail.com>
2024-06-16 13:56:43 +0530

    termial version : :

--- youtube_manager/youtube.py
+++ youtube_manager/youtube.py
@@ -1,17 +1,43 @@
 import json
 
+def list_all_videos(videos):
+  print('\n')
+  print(50*'*')
+  print(60*'*')
+  for index, video in enumerate(videos,start=1):
+    print(f"{index}. {video['name']}, Duration: {video['time']}")
+  print('\n')
+  print(60*'*')
+  print(50*'*')
 
-def list_all_videos():
-  pass
 
-def add_videos():
-  pass
+def add_videos(videos):
+  name=input("Enter video name: ")
+  time=input('Enter video duration: ')
+  videos.append({"name":name,"time":time})
+  save_data(videos)
 
-def delete_video():
-  pass
 
-def update_video():
-  pass
+def delete_video(videos):
+  list_all_videos(videos)
+  index=int(input("Enter video index to delete: "))
+  if 1<=index<=len(videos):
+    del videos[index-1]
+  else:
+    print("Enter valid index!")
+
+
+def update_video(videos):
+  list_all_videos(videos)
+  index=int(input("Enter video index to update: "))
+  if 1<=index<=len(videos):
+    new_name=input("Enter new name: ")
+    new_duration=input("Enter new duration: ")
+    videos[index-1]={"name":new_name,"time":new_duration}
+    save_data(videos)
+  else:
+    print("Enter valid index !")
+
 
 def load_data():
   try:
@@ -28,14 +54,9 @@ def save_data(videos):
          json.dump(videos,file)
 
 
-  
-
-
 def main():
 
-  videos=load_videos()
-  video=videos[0]
-
+  videos=load_data()
   while True:
     # options
     print("\n Youtube Manager| Choose an option ")
@@ -46,16 +67,16 @@ def main():
     print("5. Exit")
 
     choice=int(input("Enter your choice:"))
-
+    # print(videos)
     match choice:
       case 1:
         list_all_videos(videos)
       case 2:
         add_videos(videos)
       case 3:
-        delete_video(video)
+        delete_video(videos)
       case 4:
-        update_video(video)
+        update_video(videos)
       case 5:
         break
       case _:
@@ -65,6 +86,3 @@ def main():
 if __name__=='__main__':
   main()
 
-
-
-  
